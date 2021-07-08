9c461568370c6b912d0258b8f54828dda49b2c95
Jaivin Wylde <jaivinwylde@icloud.com>
2021-07-08 10:45:49 -0700

    abstracted the slider styles

---
+++ src/components/slider.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+const Slider = styled.input`
+  -webkit-appearance: none;
+  width: 150px;
+  margin: 10px 0 10px 0;
+  height: 25px;
+  background: #202020;
+  outline: none;
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    background: #2e2e2e;
+    cursor: pointer;
+  }
+
+  &::-moz-range-thumb {
+    width: 25px;
+    height: 25px;
+    background: #2e2e2e;
+    cursor: pointer;
+  }
+`
+
+export default Slider

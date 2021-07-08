db662122abc1d93cd38ecab6542c895261f237b8
Jaivin Wylde <jaivinwylde@icloud.com>
2021-07-08 10:46:07 -0700

    made a wrapper that tilts it's children on mouse move

---
+++ src/components/tiltWrapper.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from "react"
+import styled from "styled-components"
+import useMousePos from "../hooks/useMousePos"
+
+const Container = styled.div`
+  transform-style: preserve-3d;
+`
+
+function TiltWrapper({ children }) {
+  const maxRotation = 25
+
+  //Hooks
+  const { x: mouseX, y: mouseY } = useMousePos()
+  const [rotation, setRotation] = useState({ x: 0, y: 0 })
+  const container = useRef(null)
+
+  useEffect(() => {
+    const { x, y, width, height } = container.current.getBoundingClientRect()
+
+    let xPercent = (mouseY - (y + height / 2)) / (height / 2)
+    xPercent = Math.min(xPercent, 1)
+    xPercent = -Math.max(xPercent, -1)
+
+    let yPercent = (mouseX - (x + width / 2)) / (width / 2)
+    yPercent = Math.min(yPercent, 1)
+    yPercent = Math.max(yPercent, -1)
+
+    if (xPercent > -1 && xPercent < 1 && yPercent > -1 && yPercent < 1) {
+      setRotation({ x: xPercent * maxRotation, y: yPercent * maxRotation })
+    } else {
+      setRotation({ x: 0, y: 0 })
+    }
+  }, [mouseX, mouseY])
+
+  return (
+    <Container
+      ref={container}
+      style={{
+        transform:
+          `perspective(800px) rotateX(${rotation.x}deg) ` +
+          `rotateY(${rotation.y}deg)`,
+      }}
+    >
+      {children}
+    </Container>
+  )
+}
+
+export default TiltWrapper

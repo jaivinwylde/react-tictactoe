aca9bb964b73c0b6fd4e1320e841cfb09389cf7e
Jaivin Wylde <jaivinwylde@icloud.com>
2021-07-09 08:14:51 -0700

    simplifying even more by taking out the useEffect

--- src/components/TiltWrapper.jsx
+++ src/components/TiltWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useRef, useState } from "react"
 import styled from "styled-components"
 
 const Container = styled.div`
@@ -8,26 +8,20 @@ const Container = styled.div`
 
 export default function TiltWrapper({ children, maxRotation = 15 }) {
   //Hooks
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
   const [rotation, setRotation] = useState({ x: 0, y: 0 })
   const container = useRef(null)
 
-  useEffect(() => {
+  const handleMouseMove = ({ clientX: mouseX, clientY: mouseY }) => {
     // Get the current container's rect and mouse position
     const { x, y, width, height } = container.current.getBoundingClientRect()
-    const { x: mouseX, y: mouseY } = mousePos
 
     // Calculate the percent that we should rotate in each dimension
 
-    // Makes numbers between -1 and 1
+    // Makes numbers between -1 and 1 for each dimension
     const xPercent = -(mouseY - (y + height / 2)) / (height / 2)
     const yPercent = (mouseX - (x + width / 2)) / (width / 2)
 
     setRotation({ x: xPercent * maxRotation, y: yPercent * maxRotation })
-  }, [mousePos, maxRotation])
-
-  const handleMouseMove = ({ clientX: x, clientY: y }) => {
-    setMousePos({ x, y })
   }
 
   return (

5b71c357ea36b6c1414c2c99905a723233449933
Jaivin Wylde <jaivinwylde@icloud.com>
2021-07-09 18:03:13 -0700

    added a styled component default theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components"
+
+export const defaultTheme: DefaultTheme = {
+  colors: {
+    primary: "#202020",
+    highlight: "#2e2e2e",
+  },
+  sizes: {
+    text: {
+      extraLarge: 72,
+      large: 64,
+      medium: 48,
+      small: 36,
+      extraSmall: 30,
+    },
+    button: {
+      width: 100,
+      height: 25,
+    },
+  },
+}
